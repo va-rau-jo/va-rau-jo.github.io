94781095cff37a149ca59e173940a8b43af883a3
Victor Araujo <victor@lunenetworks.com>
2022-01-03 08:26:41 -0800

    added experiences timeline

--- src/components/Experiences/Experiences.tsx
+++ src/components/Experiences/Experiences.tsx
@@ -1,36 +1,80 @@
-import React, { useEffect, useState } from 'react';
 import './styles.scss';
 import data from '../../data/data.json';
+import React from 'react';
+
+const logos: {[index: string]:any} = data.logos;
+
+const getCell = (entry: any, i: number) => {
+    const side = i % 2 === 0 ? '_left' : '_right';
+    return (
+        <td className='exp_item_container'>
+            <div className='exp_item'>
+                <div className={'exp_company_logo exp_company_logo' + side} >
+                    <img alt="Company logo" src={logos[entry.company.toLowerCase()]} />
+                </div>
+
+                <h5 className='exp_company_name'> {entry.company} </h5>
+
+                <div className='exp_item_labels'>
+                    <div className='exp_label_container'>
+                        <img alt="date" className='exp_label_icon' src='icons/black/calendar.png' />
+                        <span> {entry.start_date} - {entry.end_date} </span>
+                    </div>
+                    <div className='exp_label_container'>
+                        <img alt="location" className='exp_label_icon' src='icons/black/marker_outline.png' />
+                        <span> {entry.location} {entry.remote ? '(Remote)' : ''} </span>
+                    </div>
+                </div>
+                <div className='exp_item_labels'>
+                    {entry.team ? 
+                        <div className='exp_label_container'>
+                            <img alt="team" className='exp_label_icon' src='icons/black/team.png' />
+                            <span> {entry.team} </span>
+                        </div> : 
+                        null }
+                </div>
+            </div>
+            <ul className='exp_accomplishments'>
+                {entry.accomplishments.map((description: string, i: number) => (
+                    <li key={i}>
+                        <span> {description} </span>
+                    </li>
+                ))}
+            </ul>
+            <ul className='exp_technologies'>
+                {entry.technologies.map((description: string, i: number) => (
+                    <li key={i}>
+                        <span> {description} </span>
+                    </li>
+                ))}
+            </ul>
+        </td>
+    );
+}
 
 const Experiences = () => {
-    const logos: {[index: string]:any} = data.logos;
+    const timelineHeight = 400 * data.experiences.length;
     return (
-        <ul className='internships_list'>
-            {data.internships.map((entry, i) => {
-                return (
-                    <li className='internships_list_item' key={i}>
-                        <img alt="Company logo" className='internships_logo' src={logos[entry.company.toLowerCase()]} />
-                        <div>
-                            <h5 className='internships_list_item_company'>  {entry.company} </h5>
-                            <div className='internships_date_container'>
-                                <img alt="date" className='internships_icon' src='icons/black/calendar.png' />
-                                <span> {entry.start_date} - {entry.end_date} </span>
-                            </div>
-                            <div className='internships_date_container'>
-                                <img alt="location" className='internships_icon' src='icons/black/marker_outline.png' />
-                                <span> {entry.location} {entry.remote ? '(Remote)' : ''} </span>
-                            </div>
-                            {entry.team ? 
-                                <div className='internships_date_container'>
-                                    <img alt="team" className='internships_icon' src='icons/black/team.png' />
-                                    <span> {entry.team} </span>
-                                </div> : 
-                                null }
-                        </div>
-                    </li>
-                );
-            })}
-        </ul>
+        <div className='exp_container'>
+            <div className='exp_timeline' id='timeline' 
+                style={{height: timelineHeight + 'px'}}>
+            </div>
+            <table className='exp_table' cellSpacing='0'>
+                <tbody>
+                    {data.experiences.map((entry, i) => {
+                        const cell = getCell(entry, i);
+
+                        return (
+                            <tr className='exp_list_row' key={i}>
+                                {i % 2 === 0 ? cell : <td></td>}
+                                {i % 2 === 1 ? cell : <td></td>}
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
+        </div>
+        
     );
 }
 
